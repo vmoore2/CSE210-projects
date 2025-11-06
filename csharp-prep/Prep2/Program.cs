085b6a9e01595d473854c52ae0ced887c2be642f
vmoore2 <anngoates@gmail.com>
2025-11-06 11:30:24 -0700

    PrepTwoCommit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage do you have in the class?");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        string letter = "A";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"You have a {letter} in the class!");
+        
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this class, but I bet you will next time!");
+
+        }
     }
 }
